7db8b5d0d140657ebdd1d6d927c87f18d22db0f7
rafal <rafal_markowski@onet.pl>
2019-01-30 13:23:30 +0100

    Updates

---
+++ precache-manifest.b8a2d0d8a95714f7b7fcedfaf0b0f177.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c999907cf8648897a3ab",
+    "url": "/react-toggle-button/static/css/main.849a6032.chunk.css"
+  },
+  {
+    "revision": "c999907cf8648897a3ab",
+    "url": "/react-toggle-button/static/js/main.c999907c.chunk.js"
+  },
+  {
+    "revision": "961314bc1cc99a36e70f",
+    "url": "/react-toggle-button/static/js/1.961314bc.chunk.js"
+  },
+  {
+    "revision": "74ac545bc9bb55d931c7",
+    "url": "/react-toggle-button/static/js/runtime~main.74ac545b.js"
+  },
+  {
+    "revision": "ed8b85a2aaff1b3cca7eae4440c1a198",
+    "url": "/react-toggle-button/index.html"
+  }
+];
